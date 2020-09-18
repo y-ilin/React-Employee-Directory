af59c98efd5a0d3a99d8a32c1719fb0e8f9b15b2
Yi Lin Lee <yilinlee19@gmail.com>
2020-09-19 01:39:32 +1000

    convert to using component for each employee row

--- src/components/employeeTable.js
+++ src/components/employeeTable.js
@@ -1,35 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import "./style.css";
-import employeeData from "../employeeData.json";
+import EmployeeData from "../employeeData.json";
+import EmployeeRow from "./employeeRow";
 
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
 function EmployeeTable(props) {
+
     const renderEmployees = () => {
+        let employeeArray = EmployeeData;
+        if (props.filter != "") {
+            employeeArray = props.filter;
+        };
+
         return (
-            employeeData.map(employee => (
-                <tr>
-                    <td><img src={employee.photo} alt="employee photo"/></td>
-                    <td>{employee.name}</td>
-                    <td>{employee.location}</td>
-                    <td>{employee.role}</td>
-                    <td>{employee.email}</td>
-                    <td>{employee.phoneNumber}</td>
-                </tr>
+            employeeArray.map(employee => (
+                <EmployeeRow
+                    key={employee.email}
+                    photo={employee.photo}
+                    name={employee.name}
+                    location={employee.location}
+                    role={employee.role}
+                    email={employee.email}
+                    phoneNumber={employee.phoneNumber}
+                />
             ))
         );
     }
 
     return (
         <table className="employeeTable">
-            <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Location</th>
-                <th>Role</th>
-                <th>Email</th>
-                <th>Phone Number</th>
-            </tr>
-            {renderEmployees()}
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Location</th>
+                    <th>Role</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderEmployees()}
+            </tbody>
         </table>
     );
 }

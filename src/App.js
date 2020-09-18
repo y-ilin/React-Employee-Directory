5e833695e5a03ea21b27497c9f9e7f7f91283e2f
Yi Lin Lee <yilinlee19@gmail.com>
2020-09-18 23:29:02 +1000

    import components to App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import logo from './logo.svg';
 import './App.css';
+import PageHeader from "./components/pageHeader";
+import EmployeeTable from "./components/employeeTable";
 
 function App() {
+  const [filterState, setFilterState] = useState("no filter");
+  const [sortState, setSortState] = useState("no sort");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <PageHeader />
+      <EmployeeTable 
+        filter={filterState}
+        sort={sortState}
+      />
+    </>
   );
 }
 

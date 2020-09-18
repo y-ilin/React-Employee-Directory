b2f411144a647f438d78e5fa3adae46576b2d284
Yi Lin Lee <yilinlee19@gmail.com>
2020-09-19 01:39:48 +1000

    functionality to live filter for employee name

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 // import logo from './logo.svg';
 import './App.css';
+import EmployeeData from "./employeeData.json";
 import PageHeader from "./components/pageHeader";
 import EmployeeTable from "./components/employeeTable";
+import SearchForm from "./components/searchForm";
 
 function App() {
-  const [filterState, setFilterState] = useState("no filter");
-  const [sortState, setSortState] = useState("no sort");
+  const [filterState, setFilterState] = useState("");
+  // const [sortState, setSortState] = useState("no sort");
+  const [searchState, setSearchState] = useState("");
+
+  const filterEmployees = () => {
+    const filteredEmployees = EmployeeData.filter(employee => {
+      const name = employee.name.toLowerCase();
+      return name.includes(searchState);
+    }); 
+    setFilterState(filteredEmployees);
+    console.log("filtered employees: ", filterState)
+  }
+
+  useEffect(() => {
+    if (!searchState) {
+      return;
+    }
+    // setFilterState(searchState);
+    filterEmployees();
+    console.log(searchState);
+  }, [searchState])
+
+  const handleInputChange = e => {
+    setSearchState(e.target.value);
+  }
 
   return (
     <>
       <PageHeader />
+      <SearchForm
+        handleInputChange={handleInputChange}
+        // value={searchState}
+      />
       <EmployeeTable 
         filter={filterState}
-        sort={sortState}
+        // sort={sortState}
       />
     </>
   );

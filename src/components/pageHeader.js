4f75790064bb73aef7c8b9bf7975cfe4896c9daf
Yi Lin Lee <yilinlee19@gmail.com>
2020-09-18 23:28:08 +1000

    add header component for page

---
+++ src/components/pageHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import "./style.css";
+
+// Using the datalist element we can create autofill suggestions based on the props.breeds array
+function PageHeader(props) {
+  return (
+    <div className="pageHeader">
+        <h1>Employee Directory</h1>
+    </div>
+  );
+}
+
+export default PageHeader;

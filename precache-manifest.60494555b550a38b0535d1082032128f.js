f10d7ff743adf80e2e769228c3ab29a7ad8c47dc
Yi Lin Lee <yilinlee19@gmail.com>
2020-09-21 00:26:26 +1000

    Updates

---
+++ precache-manifest.60494555b550a38b0535d1082032128f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e21b58ca4e6118c43d7afacc2e65506",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "8b577667d714642c6207",
+    "url": "/React-Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9e26c0bec69ac58c6ebc",
+    "url": "/React-Employee-Directory/static/css/main.9f1033b5.chunk.css"
+  },
+  {
+    "revision": "8b577667d714642c6207",
+    "url": "/React-Employee-Directory/static/js/2.4e6fe8a4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Employee-Directory/static/js/2.4e6fe8a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e26c0bec69ac58c6ebc",
+    "url": "/React-Employee-Directory/static/js/main.2bb4438f.chunk.js"
+  },
+  {
+    "revision": "d5a550fc769b1b6308d1",
+    "url": "/React-Employee-Directory/static/js/runtime-main.f187368d.js"
+  }
+]);
